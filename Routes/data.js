4dc3a1fa9461aec2f245affa47b16d71f66decba
bhashkar <bhashkar2600@gmail.com>
2022-12-24 12:06:49 +0530

    fdsds

--- Routes/data.js
+++ Routes/data.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const server = require("..//Controllers/data");
+const server1 = require("../Controllers/data1")
 
 const datarouter = express.Router();
+const datarouter1 = express.Router();
 
 datarouter.route("/products").get(server.apicontroller);
+datarouter1.route("/men").get(server1.apicontroller1);
 
-module.exports = datarouter;
+module.exports = {datarouter,datarouter1};
--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express=require("express")
 
 const app=express();
 
-const datarouter=require("./Routes/data");
+const {datarouter,datarouter1}=require("./Routes/data");
 const cors=require("cors");
 app.use(cors());
 
 app.use("/api",datarouter);
+app.use("/api",datarouter1);
 
 app.listen(process.env.PORT || 8080,()=>{
     console.log("app is running");
