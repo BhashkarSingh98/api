67cd9c48daea37beb9d0c47198b8812f951218e0
bhashkar <bhashkar2600@gmail.com>
2022-12-24 13:19:01 +0530

    edwed

--- Routes/data.js
+++ Routes/data.js
@@ -3,7 +3,7 @@ const server = require("..//Controllers/data");
 const server1 = require("../Controllers/data1")
 const server2 = require("../Controllers/women")
 const server3 = require("../Controllers/kids")
-const server3 = require("../Controllers/alldata")
+const server4 = require("../Controllers/alldata")
 
 const datarouter = express.Router();
 const datarouter1 = express.Router();
@@ -15,6 +15,6 @@ datarouter.route("/products1").get(server.apicontroller);
 datarouter1.route("/men").get(server1.apicontroller1);
 datarouter1.route("/women").get(server2.apicontroller2);
 datarouter1.route("/kids").get(server3.apicontroller3);
-datarouter1.route("/products").get(server3.apicontroller3);
+datarouter1.route("/products").get(server4.apicontroller4);
 
 module.exports = {datarouter,datarouter1,datarouter2,datarouter3,datarouter4};
