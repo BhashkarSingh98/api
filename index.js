e6684525bddd5b9f46e96e23072fe1fdb6986720
bhashkar <bhashkar2600@gmail.com>
2022-12-25 17:48:55 +0530

    sdasda

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express=require("express")
-
+const cors=require("cors");
+app.use(cors());
 const app=express();
 
 const {datarouter,datarouter1,datarouter2,datarouter3,datarouter4}=require("./Routes/data");
-const cors=require("cors");
-app.use(cors());
 
 app.use("/api",datarouter);
 app.use("/api",datarouter1);
